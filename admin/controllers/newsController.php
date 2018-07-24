a9dbd6e22deb14d31f39a29d74f86a7764fe4a57
Malinda Ranesh <malinda@omobio.net>
2018-07-24 17:16:25 +0530

    Seconde commit

---
+++ admin/controllers/newsController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omobio
+ * Date: 24/07/2018
+ * Time: 5:01 PM
+ */
---
+++ admin/pages/add_news.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Omobio
+ * Date: 24/07/2018
+ * Time: 4:46 PM
+ */
